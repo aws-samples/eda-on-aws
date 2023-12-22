311ace2585e8a1240d639fb6a80835d850be84fe
Brian Zambrano <brianz@gmail.com>
2023-12-22 16:47:58 -0700

    Clean up some errors and components
    Why
    ---
    
    - Build was broken b/c of changing name with the ProfileCard
    
    How
    ---
    
    - Refactor ProfileCard so that it can take multiple authors
    - Update all ProfileCards to use the new format
    - Reformatting with prettier
    - Remove AWS logo
    - Add format on save VSCode setting

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,67 +1,98 @@
 import React from "react"
 
-import BrianHeadshotUrl from "@site/static/img/bz-hiking.jpeg"
-import JomafeHeadshotUrl from "@site/static/img/jomafe_photo.jpg"
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLinkedin, faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons"
+import useBaseUrl from "@docusaurus/useBaseUrl"
 
-const ProfileCard = ({
-  name,
-  title,
-  headshotImage,
-  linkedInUsername,
-  twitterUsername,
-  gitHubUsername,
-}) => {
-  return (
-    <figure className="profile-card">
-      <div className="profile-image">
-        <img src={headshotImage} alt={`${name}, ${title}`} />
+const AuthorProfile = (author) => (
+  <span>
+    <div className="profile-image">
+      <img src={useBaseUrl(author.headshotImage)} alt={`${author.name}, ${author.title}`} />
+    </div>
+    <figcaption>
+      <h3>{author.name}</h3>
+      <h5>{author.title}</h5>
+      <div className="icons">
+        {author.linkedInUsername && (
+          <a href={`https://www.linkedin.com/in/${author.linkedInUsername}/`} target="_new">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        )}
+        {author.gitHubUsername && (
+          <a href={`https://github.com/${author.gitHubUsername}`} target="_new">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        )}
+        {author.twitterUsername && (
+          <a href={`https://twitter.com/${author.twitterUsername}`} target="_new">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        )}
       </div>
-      <figcaption>
-        <h3>{name}</h3>
-        <h5>{title}</h5>
-        <div className="icons">
-          {linkedInUsername && (
-            <a href={`https://www.linkedin.com/in/${linkedInUsername}/`} target="_new">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-          )}
-          {gitHubUsername && (
-            <a href={`https://github.com/${gitHubUsername}`} target="_new">
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-          )}
-          {twitterUsername && (
-            <a href={`https://twitter.com/${twitterUsername}`} target="_new">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-          )}
-        </div>
-      </figcaption>
-    </figure>
-  )
+    </figcaption>
+  </span>
+)
+
+const ProfileCard = ({ authors }) => {
+  return <figure className="profile-card">{authors.map(AuthorProfile)}</figure>
 }
 
-export const BrianProfileCard = () => (
-  <ProfileCard
-    name="Brian Zambrano"
-    title="Sr. Specialist Solutions Architect, Serverless"
-    headshotImage={BrianHeadshotUrl}
-    gitHubUsername="brianz"
-    linkedInUsername="brianzambrano"
-    twitterUsername="brianzambrano"
-  ></ProfileCard>
-)
+/**
+ *
+ Add author profile cards here
+ *
+*/
 
-export const JomafeProfileCard = () => (
-  <ProfileCard
-    name="Jordi Macià"
-    title="Sr. Solutions Architect, CPG"
-    headshotImage={JomafeHeadshotUrl}
-    gitHubUsername="jomafe"
-    linkedInUsername="jordimacia"
-    twitterUsername=""
-  ></ProfileCard>
-)
+export const BrianProfileCard = () => {
+  const author = {
+    name: "Brian Zambrano",
+    title: "Sr. Specialist Solutions Architect, Serverless",
+    headshotImage: "/img/bz-hiking.jpeg",
+    gitHubUsername: "brianz",
+    linkedInUsername: "brianzambrano",
+    twitterUsername: "brianzambrano",
+  }
+  return <ProfileCard authors={[author]} />
+}
+
+export const JomafeProfileCard = () => {
+  const author = {
+    name: "Jordi Macià",
+    title: "Sr. Solutions Architect, CPG",
+    headshotImage: "/img/jomafe_photo.jpg",
+    gitHubUsername: "jomafe",
+    linkedInUsername: "jordimacia",
+  }
+  return <ProfileCard authors={[author]} />
+}
+
+export const ProfileCardTometich = () => {
+  const author = {
+    name: "Kurt Tometich",
+    title: "Sr. Solutions Architect",
+    headshotImage: "/img/tometich-profile.jpg",
+    gitHubUsername: "boomtown15",
+    linkedInUsername: "kurt-tometich",
+    twitterUsername: "boomtown15",
+  }
+
+  return <ProfileCard authors={[author]} />
+}
+
+export const RodrigoRogelioProfileCard = () => {
+  const rodcab = {
+    name: "Rodrigo Cabrera",
+    title: "Sr. Solutions Architect",
+    headshotImage: "/img/rodcab_badge.jpg",
+    gitHubUsername: "rodcab1",
+    linkedInUsername: "rodrigo-cabrera-pliego-75455755",
+    twitterUsername: "Rodrigo11033869",
+  }
+  const rogelio = {
+    name: "Rogelio Ramirez",
+    title: "Solutions Architect",
+    headshotImage: "/img/photo_rogelio.jpg",
+    linkedInUsername: "rogelio-ramirez-rubio",
+  }
+  return <ProfileCard authors={[rodcab, rogelio]} />
+}
