2220fcd3e17b01fd226421e48aa0ef693221838d
Brian Zambrano <brianz@gmail.com>
2023-12-26 15:19:38 -0700

    Updates to CSS for dark theme + title/desc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,8 +39,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`Learn ${siteConfig.title}`}
+      description="This site offers guidance, patterns, and examples for designing, building, and operating event-driven architectures using AWS."
     >
       <HomepageHeader />
       <main>
